bcdf88044e0b1113187cba2bef6999957121f7c6
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-01-29 15:41:09 +0400

    mobile version

---
+++ cardComponent.js
@@ -0,0 +1,38 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<link rel="style" href="styleCard.css" />
+<link href="styleCard.css" rel="stylesheet" type="text/css">  
+    <div class="card-container">
+        <h1></h1>
+        <p></p>
+        <div class="img">
+            <img>
+        <div/>
+    </div>
+`;
+
+class CardFeature extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.shadowRoot.querySelector('h1').innerText = this.getAttribute(
+            'title'
+        );
+
+        this.shadowRoot.querySelector('p').innerText = this.getAttribute(
+            'text'
+        );
+
+        this.shadowRoot.querySelector('img').src = this.getAttribute('image');
+
+        this.shadowRoot.querySelector(
+            '.card-container'
+        ).style.borderTop = `solid ${this.getAttribute('border-color')} 4px`;
+    }
+}
+
+window.customElements.define('card-feature', CardFeature);
